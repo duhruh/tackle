aea72e907d8a676b6b7067361a51294bdd2be29d
David Rivera <David.R.Rivera193@gmail.com>
2017-09-24 17:21:52 -0700

    config

---
+++ config.go
@@ -0,0 +1,10 @@
+package tackle
+
+import "github.com/go-kit/kit/log/level"
+
+type Config interface {
+	HttpBindAddress() string
+	GrpcBindAddress() string
+	LogOption() level.Option
+	Environment() Environment
+}
